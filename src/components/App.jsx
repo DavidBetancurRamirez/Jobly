311ccf94c442147c9166bf2e2b6426ed5b212a11
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2024-02-17 12:51:05 -0500

    Agregando paleta de colores

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import '../styles/App.css'
 
 const App = () => {
   return (
---
+++ src/styles/colores.js
@@ -0,0 +1,10 @@
+const colores = {
+    azulOscuro: '#0C213C',
+    azulMedio: '#1262A7',
+    azulClaro: '#3D8DD2',
+    grisClaro: '#BDD3E5',
+    azulGris: '#19466D',
+    grisOscuro: '#94B0C9'
+};
+
+export default colores;
