e924de916a103ab626cf61c691bd79608dabcf2a
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2024-03-29 18:16:34 -0500

    Mantener sesion iniciada por defecto

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -6,7 +6,7 @@ const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
     const [auth, setAuth] = useState({})
-    const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist")) || false)
+    const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist")) || true)
 
     const signOut = async () => {
         setAuth({})
