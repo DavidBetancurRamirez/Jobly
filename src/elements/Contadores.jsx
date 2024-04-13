aab1ef81863ddaf0ff69566c6349c614ef3498ae
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2024-04-12 21:26:49 -0500

    Los contadores empiezan cuando se muestran en pantalla y terminan al mismo tiempo

--- src/elements/Contadores.jsx
+++ src/elements/Contadores.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
 import styled from 'styled-components';
 import colores from '../styles/colores';
 
 const Contenedor = styled.section`
     display: flex;
     justify-content: space-evenly;
+
+    @media (max-width: 550px) {
+        flex-direction: column;
+        justify-content: center;
+    }
 `
 const Registrados = styled.article`
     color: ${colores.principal};
@@ -16,31 +23,37 @@ const Registrados = styled.article`
 
     div { font-size: 2rem; }
     p { font-size: 1.5rem; }
+
+    @media (max-width: 550px) {
+        margin: 10px;
+    }
 `
 
 const Contadores = ({ estudiantes, empresas, tiempo=3 }) => {
     const [contador1, setContador1] = useState(0);
     const [contador2, setContador2] = useState(0);
     const pasos = tiempo * 60;
-    const incremento1 = estudiantes / pasos; // Incremento para llegar al máximo de pasos (tiempo en segundos)
+    const incremento1 = estudiantes / pasos;
     const incremento2 = empresas / pasos;
+    const [ref, inView] = useInView({ threshold: 0 });
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            // Incrementar ambos contadores hasta su máximo de pasos
-            if (contador1 < estudiantes) {
+        if (inView) {
+            const interval = setInterval(() => {
+              if (contador1 < estudiantes) {
                 setContador1((prevContador) => Math.min(prevContador + incremento1, estudiantes));
-            }
-            if (contador2 < empresas) {
+              }
+              if (contador2 < empresas) {
                 setContador2((prevContador) => Math.min(prevContador + incremento2, empresas));
-            }
-        }, 1000 / 60); // Ajustar para que haya la cantidad de pasos en el tiempo pasado en segundo
-
-        return () => clearInterval(interval);
-    }, [contador1, contador2, incremento1, incremento2, estudiantes, empresas]);
+              }
+            }, 1000 / pasos);
+      
+            return () => clearInterval(interval);
+          }
+    }, [inView, contador1, contador2, incremento1, incremento2, estudiantes, empresas, pasos]);
 
     return (
-        <Contenedor>
+        <Contenedor ref={ref}>
             <Registrados>
                 <div>+ {Math.floor(contador1)}</div>
                 <p>Estudiantes registrados</p>
