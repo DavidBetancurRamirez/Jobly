6ecd85d87dfb7539c2c09e9fe223cde4d9223659
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2024-04-12 21:26:04 -0500

    Agregando boton desplegable con info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,18 @@ import VideoSec from "../elements/VideoSec";
 import Footer from "../elements/Footer";
 import Navbar from "../elements/Navbar";
 import Contadores from '../elements/Contadores';
+import BtnDesplegable from '../elements/BtnDesplegable';
 
+const info = {
+  medioTiempo: {
+    titulo: "Medio tiempo",
+    contenido: "Son ofertas laborales menores a 40 horas semanales. Por ejemplo: Community manager, Business developer, Setter, Asistente virtual, etc."
+  },
+  esporadicos: {
+    titulo: "Esporadicos",
+    contenido: "Son ofertas laborales eventuales, o por proyecto y tienen fecha finalización. Por ejemplo: Mesero, Staff de evento, Edición de video, Inventario, etc."
+  }
+}
 
 const App = () => {
     return (
@@ -13,13 +24,15 @@ const App = () => {
 
           <div className="img-bg">
             <Home />
-            {/* <VideoSec /> */}
+            <VideoSec />
           </div>
+
+          <BtnDesplegable info={info.medioTiempo} />
+          <BtnDesplegable info={info.esporadicos} />
 
           <Contadores 
               estudiantes={5000}
               empresas={100}
-              tiempo={3}
           />
 
           <Footer />
